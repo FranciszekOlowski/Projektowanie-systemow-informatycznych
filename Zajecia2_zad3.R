e15242fbcc2a43941dfe5f3daf3e6055b4ed274e
FranciszekOlowski <f.olowski@student.uw.edu.pl>
2025-03-02 19:28:56 +0100

    Update Zajecia2_zad3.R

--- Zajecia2_zad3.R
+++ Zajecia2_zad3.R
@@ -1,18 +1,17 @@
-# Wczytaj dane tekstowe
 # Wczytaj plik tekstowy z lokalnego dysku
 text <- readLines(file.choose())
 text
 
+
+# Tworzenie chmury słów za pomocą pakietu wordcloud
+install.packages("wordcloud")
+library(wordcloud)# Wczytaj dane tekstowe
 
 frequent_terms <- freq_terms(text)
 frequent_terms
 frequent_terms <- freq_terms(text, stopwords = Top200Words)
 plot(frequent_terms)
 
-
-# Tworzenie chmury słów za pomocą pakietu wordcloud
-install.packages("wordcloud")
-library(wordcloud)
 
 # Utwórz chmurę słów
 wordcloud(frequent_terms$WORD, frequent_terms$FREQ)
